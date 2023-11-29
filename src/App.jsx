df18c996cfe1613e19ef487077ba4eb765f982c8
Jose Villadolid <jvil377@wgu.edu>
2023-11-29 14:34:25 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./app.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+}
+
+export default App;
