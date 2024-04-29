09a5d1a320204efa0022bbe28d039ea4b21fe12f
zetakyr <antonmiguelvilladolid@gmail.com>
2024-04-28 17:57:21 -0700

    working on the edit page and css updates

--- src/pages/DailyTaskPool.jsx
+++ src/pages/DailyTaskPool.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 const DailyTaskPool = () => {
+  const [view, setView] = useState("mandatory");
+
+  const renderTasks = () => {
+    switch (view) {
+      case "optional":
+        return <></>;
+
+      default:
+        return <></>;
+    }
+  };
+
   return (
     <div className="rightContainer">
+      <div className="rc-Child">
+        <div
+          className={`mandatoryOptional mandatory ${
+            view === "mandatory" ? "currentTab" : ""
+          }`}
+          onClick={() => {
+            setView("mandatory");
+          }}
+        ></div>
+        <div
+          className={`mandatoryOptional optional ${
+            view === "optional" ? "currentTab" : ""
+          }`}
+          onClick={() => {
+            setView("optional");
+          }}
+        ></div>
+      </div>
       <div className="tasks"></div>
     </div>
   );
